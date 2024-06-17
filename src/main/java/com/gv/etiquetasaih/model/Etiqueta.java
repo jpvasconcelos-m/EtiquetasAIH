b0941248a2694295b7cdda9b777abe66c3e29a1a
aloisio.gajesus <glarksonandrade@gmail.com>
2024-06-17 18:54:52 -0300

    Continuos

--- src/main/java/com/gv/etiquetasaih/model/Etiqueta.java
+++ src/main/java/com/gv/etiquetasaih/model/Etiqueta.java
@@ -1,9 +1,15 @@
 package com.gv.etiquetasaih.model;
 
 
-//Exemplo Etiqueta: 26(codUnidade) 24(Ano) 10190148(DigitoCrescente) 7(DigitoCiclico) -----> 262410190148-7
+//Exemplo Etiqueta: 26() 24(Ano) 10190148(DigitoCrescente) 7(DigitoCiclico) -----> 262410190148-7
 
-public record Etiqueta(int codUnidade, int ano, long digitoCrescente, int digitoCiclico) {
+public record Etiqueta(int unitCode, int year, long increasingDigit, int cyclicDigit) {
 
 
+    boolean verifCyclic(boolean vc) {
+        if (cyclicDigit == 0) {
+            System.out.println("DIGITE A ULTIMA AIH IMPRESSA");
+        }
+        return vc = false;
+    }
 }
---
+++ src/main/java/com/gv/etiquetasaih/model/Operates.java
@@ -0,0 +1,20 @@
+package com.gv.etiquetasaih.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Operates {
+    public static void main(String[] args) {
+
+
+        List<Integer> cyclics = Arrays.asList(0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Etiqueta init = new Etiqueta(26, 24, 11028345, 1);
+
+        Etiqueta fin = new Etiqueta(26, 24, 11028350, 6);
+
+      //  System.out.println(init.verifCyclic());
+
+
+    }
+}
