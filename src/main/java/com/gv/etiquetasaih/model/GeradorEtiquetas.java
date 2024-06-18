64b0fba1ffa0d8a3d9fb2ae0bf0ee395a1fd9098
joao.mendes <joaopedrosao@gmail.com>
2024-06-18 07:02:04 -0300

    adicionado metodo para teste de geracao de multiplas etiquetas

--- src/main/java/com/gv/etiquetasaih/model/GeradorEtiquetas.java
+++ src/main/java/com/gv/etiquetasaih/model/GeradorEtiquetas.java
@@ -19,7 +19,7 @@ public class GeradorEtiquetas {
 
     //TESTE:
     public static void main(String[] args) throws Exception {
-          testarPdf();
+          testarPdf2(1000,2000);
 
     }
 
@@ -46,6 +46,41 @@ public class GeradorEtiquetas {
 
 
 
+    }
+
+    private static void testarPdf2(int numero1, int numero2) throws Exception{
+        //Configura tamanho da página:
+        Document document = new Document(new Rectangle(42f,98f),0,0,4,0);
+        //Rotaciona para paisagem:
+        document.setPageSize(document.getPageSize().rotate());
+        //Cria um arquivo pdf:
+        PdfWriter.getInstance(document, new FileOutputStream("teste.pdf"));
+
+        document.open();
+
+        for (int i = 0; i < (numero2-numero1) ; i++) {
+
+            int temp = numero1 +i;
+
+            Paragraph paragraph1 = new Paragraph(temp);
+            paragraph1.setAlignment(Paragraph.ALIGN_CENTER);
+            document.add(paragraph1);
+           paragraph1 = new Paragraph(temp + " - copia");
+           document.newPage();
+            document.add(paragraph1);
+
+
+            Paragraph paragraph2 = new Paragraph(temp + " - Cópia");
+            paragraph2.setAlignment(Paragraph.ALIGN_CENTER);
+            document.add(paragraph2);
+
+        }
+
+
+
+        Desktop.getDesktop().open(new File("teste.pdf"));
+
+        document.close();
     }
 
 
