4110f9375bfbb64f50e3487c119d5ce3809f8e2d
aloisio.gajesus <glarksonandrade@gmail.com>
2024-06-21 11:24:54 -0300

    ALogical code update.

--- src/main/java/com/gv/etiquetasaih/model/Operates.java
+++ src/main/java/com/gv/etiquetasaih/model/Operates.java
@@ -1,7 +1,5 @@
 package com.gv.etiquetasaih.model;
 
-import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,43 +10,33 @@ public class Operates {
 
         Predicate<Etiqueta> isCyclics = cyclic -> cyclic.cyclicDigit() == 0;
         List<Integer> cyclics = Arrays.asList(0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-        ArrayList<Long> hgvAihs = new ArrayList<>();
+        ArrayList<Long> hgvAihsLong = new ArrayList<>();
+        ArrayList<String> hgvAih =new ArrayList<>();
 
         Etiqueta init = new Etiqueta(26, 24, 11028345, 1);
         Etiqueta fin = new Etiqueta(26, 24, 11028350, 6);
 
+        boolean c1 = isCyclics.test(init);
 
         long contador = init.increasingDigit();
         long contador2 = fin.increasingDigit();
 
+        while (contador <= contador2){
+                 hgvAihsLong.add(contador);
+                 String strCont = Long.toString(contador);
+                 hgvAih.add(strCont);
+                 contador ++;
+        }
 
-             while (contador <= contador2){
-                    hgvAihs.add(contador);
-                    contador ++;
-             }
 
-        boolean c1 = isCyclics.test(init);
+        if (!c1) {
 
-       int aihInitCod = (init.unitCode());
-        String codeYText = aihInitCod + "";
-        int aihYea = init.year();
-        String yeaText = aihYea + "";
-        /*long aih = init.increasingDigit();
-        long aih2 = fin.increasingDigit() ;
-        String aihText = aih + "";*/
-        int aihCiclic = init.cyclicDigit();
-        String cyclicText = aihCiclic + "";
-        String aihComp = codeYText + yeaText + aihCiclic;
-        System.out.println(aihComp);
-
-        if (c1 == false) {
-            hgvAihs.forEach(num -> System.out.println(num));
+            hgvAih.forEach(System.out::println);
 
 
         } else {
             System.out.println("Digite a ultima  AIH impressa");
         }
-
 
 
 
