7c0b00ffcae673af924812cb47ff50e3d3ee092c
aloisio.gajesus <glarksonandrade@gmail.com>
2024-06-23 18:38:43 -0300

    add new class circle list

---
+++ src/main/java/com/gv/etiquetasaih/model/Nodo.java
@@ -0,0 +1,11 @@
+package com.gv.etiquetasaih.model;
+
+public class Nodo {
+    public String dado;
+    public Nodo proximo;
+
+
+    public Nodo (String dado){
+        this.dado = dado;
+    }
+}
---
+++ src/main/java/com/gv/etiquetasaih/model/TLista.java
@@ -0,0 +1,44 @@
+package com.gv.etiquetasaih.model;
+
+public interface TLista {
+
+    default void add(String s) throws  Exception{
+
+    }
+
+    default String removeIdex(int i) throws Exception{
+        return null;
+    }
+
+    default void removeElem(String s) throws Exception{
+
+    }
+
+     default String previous(String s) throws Exception{
+        return null;
+    }
+
+     default int index(String s) throws Exception{
+        return 0;
+    }
+
+     default   String elemen(int i) throws Exception{
+        return null;
+    }
+
+     default int size(){
+        return 0;
+    }
+
+     default String first(){
+        return null;
+    }
+
+     default String last(){
+        return null;
+    }
+     default void print(){
+
+    }
+
+}
