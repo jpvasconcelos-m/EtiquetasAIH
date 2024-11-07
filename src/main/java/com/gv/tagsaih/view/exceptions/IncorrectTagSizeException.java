48e9e7b68e3b3985b420f3786cb234209913e498
joao.mendes <joaopedrosao@gmail.com>
2024-11-06 23:18:50 -0300

    Changed Exception popup text

--- src/main/java/com/gv/tagsaih/view/exceptions/IncorrectTagSizeException.java
+++ src/main/java/com/gv/tagsaih/view/exceptions/IncorrectTagSizeException.java
@@ -3,7 +3,7 @@ package com.gv.tagsaih.view.exceptions;
 public class IncorrectTagSizeException extends RuntimeException {
 
     public IncorrectTagSizeException(){
-        super("Pelo menos uma das etiquetas não contém a quantidade correta de dígitos!");
+        super("Etiqueta não contém a quantidade correta de dígitos!");
     } public IncorrectTagSizeException(String msg){
         super(msg);
     }
